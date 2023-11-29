9e756778692fb9030261b0365bba8de85cac0e24
yeahram623 <124855516+yeahram@users.noreply.github.com>
2023-11-29 10:03:54 +0900

    중간정리

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,21 @@
-import type { Preview } from "@storybook/react";
+// import type { Preview } from "@storybook/react";
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" }, 
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-};
+// const preview: Preview = {
+//   parameters: {
+//     actions: { argTypesRegex: "^on[A-Z].*" },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/i,
+//       },
+//     },
+//   },
+// };
 
-export default preview;
+// export default preview;
 
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { Center } from "@chakra-ui/react";
+import { ThemeProvider, Theme, CSSReset } from "@chakra-ui/react";
+addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
---
+++ src/components/CharkraButton/ChakraButton.stories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Button } from "@chakra-ui/core";
+export default {
+  title: "Chakra/Button",
+  component: Button,
+};
+export const Success = () => <Button variantColor="green"></Button>;
